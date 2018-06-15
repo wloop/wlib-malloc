ea40b5f3b49187ef7e0f85a95e56ea97a5783c42
Jeff Niu <jeffniu22@gmail.com>
2018-06-14 23:10:19 -0400

    Added static_pool

---
+++ include/wlib/mem/Byte.h
@@ -0,0 +1,8 @@
+#ifndef __WLIB_MEMORY_BYTE_H__
+#define __WLIB_MEMORY_BYTE_H__
+
+namespace wlp {
+    typedef unsigned char byte;
+}
+
+#endif
--- include/wlib/mem/Malloc.h
+++ include/wlib/mem/Malloc.h
@@ -1,12 +1,9 @@
 #ifndef __WLIB_MALLOC_MALLOC_H__
 #define __WLIB_MALLOC_MALLOC_H__
 
-#include <stddef.h>
+#include <stdint.h>
 
 namespace wlp {
-
-    typedef unsigned char byte;
-
     namespace mem {
         extern bool init(void *mem, size_t size);
         extern void *alloc(size_t bytes);
@@ -14,6 +11,7 @@ namespace wlp {
         extern void *calloc(size_t bytes);
         extern void *realloc(void *ptr, size_t size);
         extern void free(void *ptr);
+        extern bool deinit();
     }
 }
 
