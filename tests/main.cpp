7fb02b80ba232bb7f87dff6f8033eba47bb6741a
Jeff Niu <jeffniu22@gmail.com>
2018-06-16 15:01:51 -0400

    Added more tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,8 +3,15 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+#include <wlib/malloc>
+
+enum { POOL_SIZE = 1024 };
+
+static char pool[POOL_SIZE];
 static char buffer[64];
+static char *memory;
 static int wrt;
+static int count;
 
 void tlsf_printf(const char *str, ...) {
     va_list args;
@@ -19,13 +26,19 @@ void tlsf_assert(bool expr, const char *msg) {
 }
 
 void setup() {
+    wlp::mem::init(pool, POOL_SIZE);
+
     uart.begin(9600);
     wrt = sprintf(buffer, "%s\n", "Hello");
     uart.write(buffer, wrt);
 }
 
 void loop() {
-    delay(500);
-    wrt = sprintf(buffer, "%s\n", "Beep");
-    uart.write(buffer, wrt);
+    wrt = snprintf(nullptr, 0, "Counter: %i\n", count);
+    memory = static_cast<char *>(wlp::mem::alloc(wrt + 1));
+    sprintf(memory, "Counter: %i\n", count);
+    uart.write(memory, wrt);
+    wlp::mem::free(memory);
+    delay(1000);
+    count += 16;
 }
